2d79e0ca8382bfb5b2d1e00279f7a95efb30980b
Rafael Souza <souza_rafael.97@outlook.com>
2023-01-18 15:36:53 -0300

    feat: Classe Race

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,1 +1,20 @@
-// inicial commit
+export default abstract class Race {
+  constructor(
+    private _name: string,
+    private _dexterity: number
+  ) {}
+
+  get name () {
+    return this._name;
+  }
+
+  get dexterity () {
+    return this._dexterity;
+  }
+
+  abstract get maxLifePoints(): number;
+
+  static get createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+}
