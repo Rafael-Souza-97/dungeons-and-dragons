2196a4367c16df7e2d02c092617ccbec0b67996b
Rafael Souza <souza_rafael.97@outlook.com>
2023-01-19 14:58:28 -0300

    feat: Adiciona classe PVP

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,30 @@
+import Battle from './Battle';
+import Fighter from '../Fighter';
+
+export default class PVP extends Battle {
+  private _player1: Fighter;
+  private _player2: Fighter;
+
+  constructor(player1: Fighter, player2: Fighter) {
+    super(player1);
+    this._player1 = player1;
+    this._player2 = player2;
+  }
+
+  get player1(): Fighter { 
+    return this._player1;
+  }
+
+  get player2():Fighter {
+    return this._player2;
+  }
+
+  fight(): number {
+    if (this._player1.lifePoints > 0 && this._player2.lifePoints > 0) {
+      this._player1.attack(this._player2);
+      this._player2.attack(this._player1);
+    } 
+
+    return super.fight();
+  }
+}
