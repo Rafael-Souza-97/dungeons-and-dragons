ef6ab2763839138da6eed6e3c9e13c7003a34c18
Rafael Souza <souza_rafael.97@outlook.com>
2023-01-19 15:50:24 -0300

    feat: Adiciona arquivo index

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import Battle, { PVE, PVP } from './Battle';
+import Character from './Character';
+import Dragon from './Dragon';
+import Monster from './Monster';
+
+const player1 = new Character('Urso-de-Calcinha');
+const player2 = new Character('Urso-de-Calcinha2');
+const player3 = new Character('Urso-de-Calcinha3');
+
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+
+const monster1 = new Monster();
+const monster2 = new Dragon();
+
+const pvp = new PVP(player2, player3);
+const pve = new PVE(player1, [monster1, monster2]);
+
+const runBattles = (battles: Battle[]) => {
+  battles.forEach((battle) => {
+    battle.fight();
+  });
+};
+
+export { player1, player2, player3, monster1, monster2, pvp, pve, runBattles };
