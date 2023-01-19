1ee60bdd2d9624de7d1cc6d35b64a4b07ea6bba3
Rafael Souza <souza_rafael.97@outlook.com>
2023-01-19 10:33:58 -0300

    feat: Adiciona classe Monster

--- src/Fighter/SimpleFighter.ts
+++ src/Fighter/SimpleFighter.ts
@@ -2,5 +2,5 @@ export default interface SimpleFighter {
   lifePoints: number;
   strength: number;
   attack(enemy: SimpleFighter): void;
-  receiveDamage(amount: number): number;
+  receiveDamage(attackPoints: number): number;
 }
---
+++ src/Monster.ts
@@ -0,0 +1,33 @@
+import Fighter, { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  get lifePoints() {
+    return this._lifePoints;
+  }
+
+  get strength() {
+    return this._strength;
+  }
+
+  receiveDamage(attackPoints: number): number {
+    this._lifePoints -= attackPoints;
+
+    if (this.lifePoints <= 0) {
+      this._lifePoints = -1;
+    }
+
+    return this._lifePoints;
+  }
+
+  attack(enemy: Fighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+}
